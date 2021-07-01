de24bcbe60e1040e5daffeeaefb8ff8ec5846ca6
amperadev <49935446+selamatriadydev@users.noreply.github.com>
2021-07-01 22:45:31 +0700

    tambah api resource untuk non-tunggal di controller

--- larashop_api/app/Http/Controllers/BookController.php
+++ larashop_api/app/Http/Controllers/BookController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Book;
-use App\Http\Resources\Book as BookResource;
+use App\Http\Resources\Book as BookResource;//untuk resource api tunggal
+use App\Http\Resources\BookCollection as BookCollectionResource;//untuk resource api non-tunggal
 class BookController extends Controller
 {
     public function index()
     {
-        $books = DB::select('select * from books');
+        //query builder
+        // $books = DB::select('select * from books');
+        // return $books;
+        //eloquent
+        $books = new BookCollectionResource(Book::get());
         return $books;
     }
     public function view($id)
