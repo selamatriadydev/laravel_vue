b8fc804f07faeeae1631b34b12f55dda81c547ec
amperadev <49935446+selamatriadydev@users.noreply.github.com>
2021-07-01 21:25:53 +0700

    first commit

---
+++ larashop/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created () {
+    window.axios.get('http://127.0.0.1:8000/buku/laravel vue')
+    .then((response) => {
+    console.log(response)
+    })
+    .catch((error) => {
+      if (!error.response) {
+            // network error
+            // this.errorStatus = 'Error: Network Error';
+          console.log('Error: Network Error')
+        } else {
+            // this.errorStatus = error.response.data.message;
+          console.log(error.response.data.message)
+        }
+    })
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ larashop_api/routes/api.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('nama', function () {
+    return 'Namaku, Larashop API';
+});
+Route::post('umur', function () {
+    return 17;
+});
+Route::get('category/{id}', function ($id) {
+    $categories = [
+    1 => 'Programming',
+    2 => 'Desain Grafis',
+    3 => 'Jaringan Komputer',
+    ];
+    $id = (int) $id;
+    if($id==0) return 'Silakan pilih kategori';
+    else return 'Anda memilih kategori <b>'.$categories[$id].'</b>';
+});
+Route::get('book/{id}', function () {
+    return 'buku angka';
+})->where('id', '[0-9]+');
+Route::get('book/{title}', function ($title) {
+    return 'buku abjad';
+})->where('title', '[A-Za-z]+');
+
+//url tanpa label api
+// buka file RouteServiceProvider
+// hapus  prefix('api') di fungsi mapApiRoutes
+Route::prefix('v1')->group(function () {
+    Route::get('books', function () {
+    // Match dengan "/v1/books"
+    });
+    Route::get('categories', function () {
+    // Match dengan "/v1/categories"
+    });
+});
+
+Route::domain('{category}.larashop.id')->group(function () {
+    Route::get('book/{id}', function ($category, $id) {
+    //
+    });
+});
+use App\Http\Controllers\BookController;
+// Route::get('buku/{judul}', [BookController::class, 'cetak']);
+// Route::middleware('throttle:10,1')->group(function () {
+//     Route::get('buku/{judul}', [BookController::class, 'cetak']);
+// });
+// Route::middleware(['cors'])->group(function () {
+//         Route::get('buku/{judul}', [BookController::class, 'cetak']);
+// });
+// Route::middleware(['throttle:10,1', 'cors'])->group(function () {
+//     Route::get('buku/{judul}', [BookController::class, 'cetak']);
+// });
+Route::middleware(['throttle:10,1'])->group(function () {
+    Route::middleware(['cors'])->group(function () {
+        Route::get('buku/{judul}', [BookController::class, 'cetak']);
+    });
+});
+   
+   
+   
+   
+   
+   
