a5dc54072a45a911067112acfbeb1c5bdab9f066
amperadev <49935446+selamatriadydev@users.noreply.github.com>
2021-07-01 22:21:10 +0700

    tambah index di controllerbook

--- larashop_api/app/Http/Controllers/BookController.php
+++ larashop_api/app/Http/Controllers/BookController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+    public function index()
+    {
+        $books = DB::select('select * from books');
+        return $books;
+    }
     public function cetak($judul)
     {
         return $judul;
